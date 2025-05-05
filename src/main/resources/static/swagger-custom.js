661691273d6323a82bb4eb66100b0a435161a549
JihedGitlab <chaaben.jihed.gitlab@gmail.com>
2025-05-05 13:18:31 +0200

    ..

--- src/main/resources/static/swagger-custom.js
+++ src/main/resources/static/swagger-custom.js
@@ -1,8 +1,19 @@
 const eventSource = new EventSource('/sse/eureka-updates');
 
+//eventSource.addEventListener('eureka-update', (event) => {
+//    console.log('Received Eureka update event:', event.data);
+//    window.location.reload(); 
+//});
+
+
+let debounceTimeout;
 eventSource.addEventListener('eureka-update', (event) => {
     console.log('Received Eureka update event:', event.data);
-    window.location.reload(); // Recharger la page pour actualiser Swagger UI
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+		document.title = "Recharger la page pour actualiser Swagger UI";
+        window.location.reload(); // Recharger la page pour actualiser Swagger UI
+    }, 2000); // Attendre 1 seconde avant de recharger
 });
 
 eventSource.onerror = (error) => {
