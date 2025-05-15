f5b42c79c7839d2ae6f356b915b375f2bc4a4ad3
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-15 11:31:05 +0200

    ..

--- src/main/java/com/chj/gr/service/SwaggerService.java
+++ src/main/java/com/chj/gr/service/SwaggerService.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,16 +17,14 @@ public class SwaggerService {
 	
     private static final Logger logger = LoggerFactory.getLogger(SwaggerService.class);
     
-    private final EurekaClient eurekaClient;
-    private final RestTemplate restTemplate;
-
-    @Value("spring.application.name")
-    private String appName;
-    
-    public SwaggerService(EurekaClient eurekaClient, RestTemplate restTemplate) {
-		this.eurekaClient = eurekaClient;
-		this.restTemplate = restTemplate;
-	}
+    @Autowired
+    private EurekaClient eurekaClient;
+    @Autowired
+    @Qualifier("restTemplateSsl")
+    private RestTemplate restTemplateSSL;
+    @Autowired
+    @Qualifier("restTemplate")
+    private RestTemplate restTemplate;
 
 	public List<SwaggerResource> getSwaggerResources() {
 		logger.info("***********************************************************************************");
@@ -38,46 +37,50 @@ public class SwaggerService {
 		
         eurekaClient.getApplications().getRegisteredApplications().forEach(app -> {
         	logger.info("Processing application: {}", app.getName());
-        	
-            if (!app.getName().equalsIgnoreCase(appName /* "GR-CONF-SWAGGER-AGGREGATOR" */)) {
-            	
-                app.getInstances().forEach(instance -> {
-                	
-                	/**
-                	 * Vérifier la métadonnée swagger.aggregator.enabled:
-                	 * eureka:
-						  instance:
-						    metadata-map:
-						      swagger.aggregator.enabled: false/true ?
-                	 */
-                    String swaggerEnabled = instance.getMetadata().getOrDefault("swagger.aggregator.enabled", "false");
-                    if (!"true".equals(swaggerEnabled)) {
-                        logger.info("Skipping service {}: swagger.aggregator.enabled={}", app.getName(), swaggerEnabled);
-                        return;
-                    }
-                    
-					String[] possibleEndpoints = {
-							String.format("http://%s:%s/v3/api-docs", instance.getHostName(), instance.getPort()),
-							String.format("http://%s:%s/v2/api-docs", instance.getHostName(), instance.getPort())
-					};
-					for (String swaggerUrl : possibleEndpoints) {
-					    try {
-					    	logger.info("Attempting to fetch Swagger JSON from: {}", swaggerUrl);
-					        String swaggerJson = restTemplate.getForObject(swaggerUrl, String.class);
-					        if (swaggerJson != null) {
-					            resources.add(new SwaggerResource(app.getName(), swaggerUrl));
-					            logger.info("Successfully retrieved Swagger JSON for service: {} at {}", app.getName(), swaggerUrl);
-					            break;
-					        } else {
-	                            logger.warn("Swagger JSON is null for service: {} at {}", app.getName(), swaggerUrl);
-	                        }
-					    } catch (Exception e) {
-					        logger.error("Failed to retrieve Swagger JSON for service: {} at {}. Error: {}", 
-					            app.getName(), swaggerUrl, e.getMessage());
-					    }
-					}
-                });
-            }
+            app.getInstances().forEach(instance -> {
+            	/**
+            	 * Vérifier la métadonnée swagger.aggregator.enabled:
+            	 * eureka:
+					  instance:
+					    metadata-map:
+					      swagger.aggregator.enabled: false/true ?
+            	 */
+                String swaggerEnabled = instance.getMetadata().getOrDefault("swagger.aggregator.enabled", "false");
+                if (!"true".equals(swaggerEnabled)) {
+                    logger.info("Skipping service {}: swagger.aggregator.enabled={}", app.getName(), swaggerEnabled);
+                    logger.info("-----------------------------------------------------------------------------------");
+                    return;
+                }
+                
+				String[] possibleEndpoints = {
+						String.format("https://%s:%s/v3/api-docs", instance.getHostName(), instance.getPort()),
+						String.format("http://%s:%s/v3/api-docs", instance.getHostName(), instance.getPort()),
+						String.format("https://%s:%s/v2/api-docs", instance.getHostName(), instance.getPort()),
+						String.format("http://%s:%s/v2/api-docs", instance.getHostName(), instance.getPort())
+				};
+				for (String swaggerUrl : possibleEndpoints) {
+				    try {
+				    	String swaggerJson = null;
+				    	logger.info("Attempting to fetch Swagger JSON from: {}", swaggerUrl);
+				    	if (swaggerUrl.startsWith("https")) {
+				    		swaggerJson = restTemplateSSL.getForObject(swaggerUrl, String.class);
+						} else {
+							swaggerJson = restTemplate.getForObject(swaggerUrl, String.class);
+						}
+				        if (swaggerJson != null) {
+				            resources.add(new SwaggerResource(app.getName(), swaggerUrl));
+				            logger.info("Successfully retrieved Swagger JSON for service: {} at {}", app.getName(), swaggerUrl);
+				            break;
+				        } else {
+                            logger.warn("Swagger JSON is null for service: {} at {}", app.getName(), swaggerUrl);
+                        }
+				    } catch (Exception e) {
+				        logger.error("Failed to retrieve Swagger JSON for service: {} at {}. Error: {}", 
+				            app.getName(), swaggerUrl, e.getMessage());
+				    }
+				}
+				logger.info("-----------------------------------------------------------------------------------");
+            });
         });
         logger.info("Returning {} Swagger resources", resources.size());
         return resources;
